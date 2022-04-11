5ab6f6a596e36def4a4f81c26cb49c53f68e9bff
kchangwook <dbwlalss2!>
2022-04-11 22:30:22 +0900

    2.2 변수와 자료형 - 문자열 자료형 알아보기

---
+++ src/main/kotlin/chapter02/section2/FormattedString.kt
@@ -0,0 +1,12 @@
+package chapter02.section2
+
+fun main() {
+    val num = 10
+    val formattedString = """
+        var a = 6
+        var b = "Kotlin
+        println(a + num
+    """
+
+    println(formattedString)
+}
