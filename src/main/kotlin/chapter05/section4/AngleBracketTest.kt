41624e73c276cdce788cecfb4b170b0ef2963b1e
kchangwook <dbwlalss2!>
2022-05-20 23:44:45 +0900

    5.4 super와 this의 참조 - 인터페이스에서 참조하기

---
+++ src/main/kotlin/chapter05/section4/AngleBracketTest.kt
@@ -0,0 +1,38 @@
+package chapter05.section4
+
+open class A {
+    open fun f() {
+        println("A Class f()")
+    }
+
+    fun a() {
+        println("A Class a()")
+    }
+}
+
+interface B {
+    fun f() {
+        println("B Interface f()")
+    }
+
+    fun b() {
+        println("B Interface b()")
+    }
+}
+
+class C: A(), B {
+    override fun f() {
+        println("C Class f()")
+    }
+
+    fun test() {
+        f()
+        b()
+        super<A>.f()
+        super<B>.f()
+    }
+}
+
+fun main() {
+    C().test()
+}
