030096e33cb9110c665e0c4b68181487ffa8d299
kchangwook <dbwlalss2!>
2022-07-25 23:07:50 +0900

    8.2 배열 다루기 - 배열을 사용하는 방법

---
+++ src/main/kotlin/chapter08/section2/ArrayAccessTest.kt
@@ -0,0 +1,13 @@
+package chapter08.section2
+
+import java.util.*
+
+fun main() {
+    val array = intArrayOf(1, 2, 3, 4, 5)
+
+    println("arr: ${Arrays.toString(array)}")
+    println("size: ${array.size}")
+    println("sum(): ${array.sum()}")
+
+
+}
