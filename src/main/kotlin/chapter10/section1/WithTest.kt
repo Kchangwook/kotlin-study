30f9a8355111e359216eec230e816b5a4a7b849c
kchangwook <dbwlalss2!>
2022-09-07 00:28:39 +0900

    10.1 코틀린 표준 함수 - with() 함수 활용하기 - use() 함수 활용하기

---
+++ src/main/kotlin/chapter10/section1/WithTest.kt
@@ -0,0 +1,14 @@
+package chapter10.section1
+
+fun main() {
+    data class User(val name: String, var skills: String, var email: String? = null)
+    val user = User("Kildong", "default")
+
+    val result = with (user) {
+        skills = "Kotlin"
+        email = "kildong@example.com"
+    }
+
+    println(user)
+    println("result: $result")
+}
